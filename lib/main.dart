c9486e5d0516d400e228ecc318f0437611b640a3
sharjil-ali37 <sharjilali377@gmail.com>
2022-11-20 23:45:27 +0530

    splash_screen_icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:login/screens/home/splashScreen2.dart';
 import 'package:login/screens/signin_screen.dart';
 
 void main() async {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         fontFamily: "Open-Sans",
       ),
-      home: const SignInScreen(),
+      home: const SplashScreen(),
     );
   }
 }
